5f39f5c2d898757654a94b8f2e1a1077ada3a591
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-09 16:14:45 -0400

    Fixed path to setupdb.sql.

--- postgresql-init/setup.sh
+++ postgresql-init/setup.sh
@@ -1,3 +1,3 @@
 echo "Begin database setup..."
 
-psql -v dbuser=$DB_USER -v dbname=$DB_NAME -f setupdb.sql
+psql -v dbuser=$DB_USER -v dbname=$DB_NAME -f /opt/app-root/src/postgresql-init/setupdb.sql
