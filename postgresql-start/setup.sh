510f74a0e62a744bd9d0eef0f06cb34ca3595f7b
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-09 23:27:14 -0400

    Update setup.sh

--- postgresql-start/setup.sh
+++ postgresql-start/setup.sh
@@ -1,3 +1,3 @@
 echo "Begin database setup..."
 
-psql -v dbuser=$DATABASE_USER -v dbpassword=$DATABASE_PASSWORD -v dbname=$POSTGRESQL_DATABASE -f /opt/app-root/src/postgresql-start/setupdb.sql
+psql -v dbuser=$DATABASE_USER -v dbpassword=$DATABASE_PASSWORD -v dbname=$POSTGRESQL_DATABASE -v dbadmin=$POSTGRESQL_USER -f /opt/app-root/src/postgresql-start/setupdb.sql
